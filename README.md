0dea32756d942bb2f383b9354dc63c5f109eee96
Minyoung Kim <cyzest@nate.com>
2014-02-19 23:45:11 +0900

    Test Code Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+﻿1. 테스트를 위해선 Subscribe 가능한 Redis 서버가 준비되어 있어야 한다.
+
+2. redis-cli 툴을 이용해서 간단하게 publish 명령을 이용해 테스트 한다.
+
+	./redis-cli -a password publish "test-channel" "test message"
---
+++ subscriber.js
@@ -0,0 +1,13 @@
+﻿
+var sys = require("sys");
+var redis = require("redis");
+
+var subscriber = redis.createClient(6379, "host");
+
+subscriber.auth("password");
+
+subscriber.subscribe("test-channel");
+
+subscriber.on("message", function(channel, message){
+    sys.puts(channel + " : " + message);
+});
